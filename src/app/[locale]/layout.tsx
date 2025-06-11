9618ba5f7dde2081e8f7789cf6d539b36d6dc8da
Andrés Gómez <caangogi@gmail.com>
2025-06-11 19:18:20 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -27,7 +27,7 @@ export default async function LocaleLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased min-h-screen flex flex-col bg-background text-foreground">
+      <body className="font-body antialiased min-h-screen flex flex-col">
         <NextIntlClientProvider messages={messages}>
           {children}
           <Toaster />
