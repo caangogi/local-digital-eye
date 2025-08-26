095783836e259d5ed3a60d16f066b74d61948760
Andrés Gómez <caangogi@gmail.com>
2025-08-26 17:33:45 +0000

    ReferenceError: Auth is not defined     at useAuth (https://9000-firebas

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -121,7 +121,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 }
 
 export function useAuth(): AuthState {
-  const context = useContext(Auth.AuthContext);
+  const context = useContext(AuthContext);
   if (context === undefined) {
     throw new Error('useAuth must be used within an AuthProvider');
   }
